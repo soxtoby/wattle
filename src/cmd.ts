60bd7463a0dc8f2c5b17cfe7777e49c0f8c5aa33
Simon Oxtoby <soxtoby@zaptechnology.com>
2020-09-05 09:45:30 +1000

    Removing support for globally installing wattle

--- src/cmd.ts
+++ src/cmd.ts
@@ -1,9 +1,39 @@
 #!/usr/bin/env node
-import findup = require('findup-sync');
+import * as path from 'path';
+import { args } from './CommandLineArgs';
+import { fixWindowsPath } from "./CommandLineHelpers";
+import { ExitCodes } from './ExitCodes';
+import { LogLevel } from './LogLevel';
+import { TestRunner } from './TestRunner';
+import { combineConfigs, lastConfig } from './TestRunnerConfig';
 
-let localCommandLineRunner = findup('node_modules/wattle/lib/CommandLineRunner.js');
+try {
+    require(path.resolve(args.config));
+} catch { }
 
-if (!localCommandLineRunner)
-    throw new Error("Couldn't find local wattle installation");
+let options = combineConfigs(lastConfig, {
+    middleware: args.middleware,
+    showStacks: args.showStacks,
+    verbosity: args.verbosity && LogLevel[args.verbosity],
+    watch: args.watch,
+    buildServer: args.buildServer,
+    testFiles: (args.testFiles || (args._.length ? args._ : undefined))?.map(fixWindowsPath),
+    processCount: args.processCount,
+    tsProject: args.tsProject
+});
 
-require(localCommandLineRunner);
+let runner = new TestRunner(options);
+
+let completed: Promise<unknown> = args.watch ? runner.watch() : runner.run();
+
+completed
+    .then(() => {
+        process.exit(runner.allTestsPassed ? ExitCodes.Success : ExitCodes.TestsFailed);
+    })
+    .catch(error => {
+        if (error == ExitCodes.ErrorLoadingMiddleware)
+            return process.exit(ExitCodes.ErrorLoadingMiddleware);
+
+        console.error(error);
+        process.exit(ExitCodes.UnexpectedError);
+    });
